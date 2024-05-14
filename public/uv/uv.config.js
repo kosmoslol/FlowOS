22e30e3c5c1120cde221da5a7bd0ee166c6a3a98
kosmos <119829358+kosmoslol@users.noreply.github.com>
2024-05-13 19:05:53 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,39 +1,10 @@
-self.xor = {
-  randomMax: 100,
-  randomMin: -100,
-
-  encode: (str) => {
-    if (!str) return str
-    return encodeURIComponent(
-      str
-        .toString()
-        .split('')
-        .map((char, ind) =>
-          ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-        )
-        .join('')
-    )
-  },
-  decode: (str) => {
-    if (!str) return str
-    const [input, ...search] = str.split('?')
-
-    return (
-      decodeURIComponent(input)
-        .split('')
-        .map((char, ind) =>
-          ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
-        )
-        .join('') + (search.length ? '?' + search.join('?') : '')
-    )
-  }
-}
 self.__uv$config = {
-  prefix: '/service/',
-  bare: 'https://server.flow-works.me' + '/bare/',
-  encodeUrl: self.xor.encode,
-  decodeUrl: self.xor.decode,
-  handler: '/uv/uv.handler.js',
-  bundle: '/uv/uv.bundle.js',
-  config: '/uv/uv.config.js',
-}
+    prefix: '/service/',
+    bare: 'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
